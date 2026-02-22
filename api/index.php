338c23c872f4503bd842f5dbc12694d6c21c8a4d
SuperDuperBaim <anandaibrahimhs@gmail.com>
2026-02-22 12:33:10 +0700

    fix: explicit framework null and standard bridge

--- api/index.php
+++ api/index.php
@@ -1,4 +1,6 @@
 <?php
 
-// Forward Vercel requests to normal index.php
+// This file is the entry point for Vercel.
+// It maps the request to Laravel's public/index.php
+
 require __DIR__ . '/../public/index.php';
